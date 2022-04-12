577ce50a892117290c7d8dc17e85a10821956921
Hannah2020M <coder2020aug@gmail.com>
2022-04-12 10:28:05 -0500

    added studio class Area to the CORRECTfolderDocumentsjwde

---
+++ src/org/launchcode/java/studios/areaofacircle/Area.java
@@ -0,0 +1,13 @@
+package org.launchcode.java.studios.areaofacircle;
+import java.util.Scanner;
+
+public class Area {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of a circle: ");
+        Double radius = input.nextDouble();
+       // Double area =  3.14 * radius * radius;
+        Double area = Circle.getArea(radius);
+        System.out.println("Area of the circle = " + area);
+    }
+}
