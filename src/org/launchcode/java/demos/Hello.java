72e18534ca28fcc1bea1faddd5768408c4e6fd3d
Hannah2020M <coder2020aug@gmail.com>
2022-04-12 08:50:08 -0500

    added12apr2022 text fileIMGpushDisplayHelloWorked

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -6,10 +6,11 @@ package org.launchcode.java.demos;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        //String str = "Hello, World";
-        String checkJavaDASHweb = "Hello, 12apr2022";
+        String str = "Hello, World";
+        String str12apr2022 = "Hi world 12Apr2022";
         double a = 42;
         System.out.println(a);
+        System.out.println(str12apr2022);
         //System.out.println(a.getClass().getSimpleName());
     }
 }
