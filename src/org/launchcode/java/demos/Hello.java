8047d371a24ce0d71a1c71b3accfb3161b85efb3
Hannah2020M <coder2020aug@gmail.com>
2022-04-12 08:31:55 -0500

    checking on 12apr2022 if this PUSHes2 remoteGithubRepo

--- src/org/launchcode/java/demos/Hello.java
+++ src/org/launchcode/java/demos/Hello.java
@@ -6,7 +6,8 @@ package org.launchcode.java.demos;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, World");
-        String str = "Hello, World";
+        //String str = "Hello, World";
+        String checkJavaDASHweb = "Hello, 12apr2022";
         double a = 42;
         System.out.println(a);
         //System.out.println(a.getClass().getSimpleName());
