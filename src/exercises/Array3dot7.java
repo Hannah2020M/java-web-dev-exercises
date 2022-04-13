a1bb56679232a7366972b3c13ab19e5b2816ae61
Hannah2020M <coder2020aug@gmail.com>
2022-04-13 09:04:37 -0500

    completed working code for exercises Arrays 3dot7dot1

--- src/exercises/Array3dot7.java
+++ src/exercises/Array3dot7.java
@@ -1,20 +1,25 @@
 package exercises;
+
 import java.lang.String;
+import java.util.Arrays;
+
 public class Array3dot7 {
     public static void main(String[] args) {
         int[] arr1 = {1, 1, 2, 3, 5, 8};
-        int l = arr1.length;
+        int l1 = arr1.length;
         String drSeuss = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
-//        for (int i=0; i<l; i++) {
-//            System.out.println(arr1[i]);
-//        }//end of 1st for-each LOOP on arr1 to printALLelements of arr1
-
-        for (int i=0; i<l; i++) {
+        for (int i = 0; i < l1; i++) {
             if ((arr1[i] % 2) != 0) {
                 System.out.println(arr1[i]);
             }//end of IF loopPrintsONLYoddNum in arr1
         }//end of 1st for-each LOOP on arr1
-       String[] arr2 = drSeuss.split(" ",5);
-        System.out.println(arr2);
+        String[] arr2 = drSeuss.split(" ");
+        System.out.println(Arrays.toString(arr2));
+        System.out.println("length of arr2 = "+arr2.length);
+        String[] arr3 = drSeuss.split(",");
+        System.out.println(Arrays.toString(arr3));
+        System.out.println("length of arr3 = "+arr3.length);
+        System.out.println("         Elements of array containing the "+arr3.length+" parts  of string drSeuss :-" );
+        for (String y : arr3) System.out.println(y);
     }
 }
