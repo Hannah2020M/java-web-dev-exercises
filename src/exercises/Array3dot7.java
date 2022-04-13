c22f3e91d1d5ce713971c767d617140f8e2e1936
Hannah2020M <coder2020aug@gmail.com>
2022-04-12 23:18:56 -0500

    initial commit Array3dot7 to exercises

---
+++ src/exercises/Array3dot7.java
@@ -0,0 +1,15 @@
+package exercises;
+
+public class Array3dot7 {
+    public static void main(String[] args) {
+        int[] arr1 = {1, 1, 2, 3, 5, 8};
+//        int l = arr1.length();
+//        for (int i=0; i<l; i++){
+//
+//        }//end of 1st arr1 FOR loop
+
+        for (int i : arr1) {
+            System.out.println(arr1[i]);
+        }//end of 1st for-each LOOP on arr1
+    }
+}
