f5b5d28f6923035b966824ad61665c5fcc212587
Hannah2020M <coder2020aug@gmail.com>
2022-04-14 21:38:48 -0500

    studio3dot8CharCount

--- src/exercises/HashMap3dot7dot3stuID.java
+++ src/exercises/HashMap3dot7dot3stuID.java
@@ -2,14 +2,17 @@ package exercises;
 
 public class HashMap3dot7dot3stuID {
     public static void main (String[] args) {
-        HashMap<String, Double> students = new HashMap<>();
-        Scanner input = new Scanner(System.in);
-        String newStudent;
-        do {
-
-            System.out.print("Student: ");
-            newStudent = input.nextLine();
-        }while(!newStudent.equals(""));
-        System.out.println(student.getKey() + " (" + student.getValue() + ")");
+//        HashMap<String, Double> students = new HashMap<>();
+//        Scanner input = new Scanner(System.in);
+//        String newStudent;
+//        do {
+//
+//            System.out.print("Student: ");
+//            newStudent = input.nextLine();
+//        }while(!newStudent.equals(""));
+//        System.out.println(student.getKey() + " (" + student.getValue() + ")");
+//
+//
+  System.out.println("Finish later");
     }
 }
